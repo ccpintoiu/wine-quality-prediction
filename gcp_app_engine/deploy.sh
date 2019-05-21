c6fd4b81b8f194aed91c81aa43047c5a4a882f03
Cosmin Pintoiu <cosmin.pintoiu@gmail.com>
2019-05-21 18:25:37 +0300

    Update deploy.sh

--- gcp_app_engine/deploy.sh
+++ gcp_app_engine/deploy.sh
@@ -2,8 +2,11 @@
 
 #cd application
 pip install -r requirements.txt -t lib
-gcloud app create
-gcloud app deploy
+# uncomment below for initial app creation
+#gcloud app create
+#gcloud app deploy
+gcloud app deploy --project data-engineering-229511 -v 20190520t195634
+PROJECT=data-engineering-229511
 
 PROJECT=data-engineering-229511
 echo "Visit https://PROJECT-ID.appspot.com/  e.g. https://${PROJECT}.appspot.com"
